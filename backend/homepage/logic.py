998d9f2916bc3af06506189dca92824c70c9e290
10165101109 <10165101109@stu.ecnu.edu.cn>
2020-03-11 23:52:53 +0800

    .

--- backend/homepage/logic.py
+++ backend/homepage/logic.py
@@ -1,8 +1,28 @@
+import sys
+sys.path.append('..')
 import config
 import numpy as np
+import util.db as db
 
 def login(username, password):
-    # TODO
+    para_dict = {}
+    para_dict['select_key'] = 'id'
+    para_dict['select_value'] = []
+    para_dict['table_name'] = 'login'
+    para_dict['key'] = ['username','password']
+    para_dict['value'] = [username,password]
+    status = db.select(para_dict)
+    if status == 'ok':
+        uid = para_dict['select_value']
+
+    para_dict['select_key'] = 'career'
+    para_dict['select_value'] = []
+    para_dict['table_name'] = 'employee'
+    para_dict['key'] = 'id'
+    para_dict['value'] = uid
+    status = db.select(para_dict)
+    if status == 'ok':
+        career = para_dict['select_value']
     # if ok: return uid and career, else return status
     # status: ok, username_not_exist, password_error
     if status == 'ok':
@@ -12,26 +32,30 @@ def login(username, password):
 
 def search(project_id, project_name):
     id, name, status = 0, 1, 2 # index in db result, need to be modify
+    
+    para_dict = {}
+    para_dict['select_key'] = ['id','name','status']
+    para_dict['select_value'] = []
+    para_dict['tablename'] = 'project'
+    para_dict['key'] = []
+    para_dict['value'] = []
 
     if project_id != config.none_value:
-        # TODO
-        if project_name != config.none_value:
-            # search with both id and name, maybe empty
-            pass
-        else:
-            # search only with id
-            pass
-    elif project_name != config.none_value:
-        # TODO
-        # search by name, maybe multiple
-        pass
-    else:
+        para_dict['key'].append('id')
+        para_dict['value'].append(project_id)
+    
+    if project_name != config.none_value:
+            
+        para_dict['key'].append('name')
+        para_dict['value'].append(project_name)
+                
+    if len(para_dict['key']) == 0 or not db.select(para_dict)=='ok':
         return 'error'
 
     # used for test
     # db_result = [[1, 2, 3],
     #              [4, 5, 3]]
-
+    db_result = para_dict['select_value']
     db_result = np.array(db_result, dtype=np.str)
     return (list(db_result[:, id]), list(db_result[:, name]), list(db_result[:, status]))
 
@@ -42,16 +66,37 @@ def get_project(page, size, uid=None):
     # 3. return page * size ~ page * (size + 1)
 
     id, name, status, update_time = 0, 1, 2, 3 # index in db result, need to be modify
+    para_dict = {}
+    para_dict['select_value'] = []
+    para_dict['table_name'] = 'project'
+
     if uid is None:
-        # get all project
+        para_dict['select_key'] = '*'
         pass
     else:
-        # get uid's project
+        para_dict['select_key'] = 'id'
         pass
-    
+    db.select(para_dict)
     # used for test
     # db_result = [[1, 2, 3, '2020-03-10'],
     #              [4, 5, 3, '2020-03-11']]
 
-    db_result = np.array(db_result, dtype=np.str)
+    tmp = np.array(para_dict['select_value'], dtype=np.str)
+    db_result = np.array(tmp)
+    l = np.argsort(tmp[:,update_time])
+    for i in range(l.shape[0]):
+        db_result[l.shape[0] - 1 - l[i]] = tmp[i] 
     return (list(db_result[:, id]), list(db_result[:, name]), list(db_result[:, status]), list(db_result[:, update_time]))
+
+if __name__ == "__main__":
+    tmp = np.array([[1, 2, 3, '2020-03-10'],
+            [4, 5, 3, '2020-03-11']])
+
+    db_result = np.array(tmp)
+    l = np.argsort(tmp[:,3])
+    
+    length =  l.shape[0]
+    for i in range(length):
+        
+        db_result[length-1-l[i]] = tmp[i]
+    print(db_result)
