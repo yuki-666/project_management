d43c9c6f2d65fbf821fe76835702ef0f92cbeddd
10165101109 <10165101109@stu.ecnu.edu.cn>
2020-03-15 12:39:27 +0800

    logic&db

--- backend/approval/logic.py
+++ backend/approval/logic.py
@@ -9,58 +9,108 @@ import util.db as db
 # maybe move to another place called 'project'
 def modify_project(project_id, project_name, describe, scheduled_time, delivery_day, project_superior_name, \
     major_milestones, adopting_technology, business_area, main_function):
-    # TODO
+    
     # modify project info, search by id
-    # return 'error' if project_id not found
-    return 'ok'
+    para_dict['tablename'] = 'project'
+    #id,name,status,customer_id,main_function,domain_id,tech,project_leader_id,submit_date,reserve_date
+
+    para_dict['set_key'] = ['project_name','describe','scheduled_time','submit_date','project_leader',\
+        'major_milestones','tech','domain_id','main_function']
+    para_dict['set_value'] = [project_name, describe, scheduled_time, delivery_day, project_superior_name, \
+    major_milestones, adopting_technology, business_area, main_function]
+    para_dict['where_key'] = ['peoject_id']
+    para_dict['where_value'] = [project_id]
+
+    status = db.update(para_dict)
+    return status
 
 # maybe move to another place called 'project'
 def confirm_project(project_id, status):
-    # TODO
-    # return 'error' if project_id not found
+    para_dict['tablename'] = 'project'
+    para_dict['set_key'] = ['status']
+    para_dict['set_value'] = [status]
+    para_dict['where_key'] = ['peoject_id','status']
+    para_dict['where_value'] = [project_id,1]
+    
     # check if project status is 1 (pending), return 'error' if not
     # modify project status, from 1 to 0/2 (rejection/established)
-    return 'ok'
+    return db.update(para_dict)
 
 # maybe move to another place called 'work_time'
 def get_work_time_by_uid(uid, is_superior=False, include_finished=False):
-    # TODO
-    # not return delete label = 1
+    para_dict['select_key'] = ['work_time_id', 'employer.name', 'function_name', 'work_time.event_name', 'work_time.start_time', 'work_time.end_time']
+    para_dict['select_value'] = []
+    para_dict['tablename'] = 'work_time'
+    para_dict['join_tablename'] = ['employer']
+    para_dict['where_key'] = ['delete_label','work_time.worker_id']
+    para_dict['where_value'] = [0,'employer.id']
+    if include_finished:
+        para_dict['select_key'].append('status')
+    if is_superior :
+        para_dict['join_tablename'].append['project_participant']
+        para_dict['where_key'].append('project_participant.leader_id')
+        para_dict['where_value'].append(uid)
+        para_dict['where_key'].append('project_participant.person_id')
+        para_dict['where_value'].append('work_time.worker_id')
+    else :
+        para_dict['where_key'].append('work_time.worker_id')
+        para_dict['where_value'].append(uid)
+    db.select(para_dict)
     # is_superior=True: uid是上级的uid，要获取他所有项目下级的工时
     # is_superior=False: uid是自己的uid，获取自己所有工时
     # include_finished: whether return approved part
-
+    result = np.array(para_dict['select_value'],dtype=np.str)
     if include_finished:
-        return (work_time_id, worker_name, function_name, event_name, start_time, end_time, status)
+        return (list(result[:,0]),list(result[:,1]) , list(result[:,2]), list(result[:,3]), list(result[:,4]), list(result[:,5]), list(result[:,6]))
     else:
-        return (work_time_id, worker_name, function_name, event_name, start_time, end_time)
-
+        return (list(result[:,0]),list(result[:,1]) , list(result[:,2]), list(result[:,3]), list(result[:,4]), list(result[:,5]))
+    
 # maybe move to another place called 'work_time'
 def get_work_time_by_work_time_id(work_time_id):
-    # TODO
+    para_dict['select_key'] = ['work_time_id', 'worker_name', 'function_name', 'event_name', 'start_time', 'end_time','delete_label']
+    para_dict['select_value'] = []
+    para_dict['tablename'] = 'work_time'
+    para_dict['key'] = ['work_time_id']
+    para_dict['value'] = [work_time_id]
+    status = db.select(para_dict)
+    delete_label = para_dict['select_value'].pop()
+    if status == 'error' or delete_label == 1 :
+        return 'error'
     # return error if delete label = 1
     # check if work_time_id exist, return 'error' if not
-    return (work_time_id, worker_name, function_name, event_name, start_time, end_time)
-
+    return tuple(para_dict['select_value'])
 # maybe move to another place called 'work_time'
 def confirm_work_time(work_time_id, status):
-    # TODO
+    para_dict['tablename'] = 'work_time'
+    para_dict['set_key'] = ['status']
+    para_dict['set_value'] = [status]
+    para_dict['where_key'] = ['peoject_id','delete_label','status']
+    para_dict['where_value'] = [project_id,0,1]
+    status = db.update(para_dict)
     # return 'error' if work_time_id not found
     # return error if delete label = 1
     # check if project status is 1 (pending), return 'error' if not
     # modify project status, from 1 to 0/2 (rejection/approved)
-    return 'ok'
+    return status
 
 # maybe move to another place called 'work_time'
 def modify_word_time(work_time_id, worker_name, function_name, event_name, start_time, end_time):
-    # TODO
+    para_dict['tablename'] = 'work_time'
+    para_dict['set_key'] = ['worker_name', 'function_name', 'event_name', 'start_time', 'end_time']
+    para_dict['set_value'] = [worker_name, function_name, event_name, start_time, end_time]
+    para_dict['where_key'] = ['work_time_id','delete_label']
+    para_dict['where_value'] = [work_time_id,0]
     # modify project info, search by id
     # return 'error' if work_time_id not found
     # return error if delete label = 1
-    return 'ok'
+    return db.update(para_dict)
 
 def delete_work_time(work_time_id):
-    # TODO
+    para_dict['tablename'] = 'work_time'
+    para_dict['set_key'] = ['delete_label']
+    para_dict['set_value'] = [1]
+    para_dict['where_key'] = ['work_time_id']
+    para_dict['where_value'] = [work_time_id]
     # return 'error' if work_time_id not found
     # add delete label (set delete_label column to 1, default is 0)
-    return 'ok'
+    return db.update(para_dict)
--- backend/util/db.py
+++ backend/util/db.py
@@ -9,6 +9,22 @@ def select(para):#parameter
     #para[tablename] = 'employee'
     #para[key] = 'attribute1'
     #para[value] = 'value1'
-
+    
+    # in table'work_time' not return delete label = 1
     #to do
+    # return 'error' if key not found
     return status #'ok' or other
+
+def update(para):
+    #update project
+    #set project_name = hh,main_function = nn
+    #where project_id = 30
+    #para[tablename] = 'project'
+    #para[update_key] = 
+    #para[update_value] = 
+    #para[where_key] = 
+    #para[where_value] =
+
+    #todo
+    # return 'error' if project_id not found else return 'ok'
+    return status
