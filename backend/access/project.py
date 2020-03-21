3086af09002d88f013ddbb2e84cef615695bb219
unknown <scsse_hqh2016@126.com>
2020-03-21 15:16:18 +0800

    Refactor approval part.

--- backend/access/project.py
+++ backend/access/project.py
@@ -10,15 +10,15 @@ import config
 project_access = Blueprint('project_access', __name__)
 
 @project_access.route('/mine', methods=['GET'])
-def mine():
+def project_mine():
     request_data = get_value_dict()
     if not check_dict(request_data, ['uid', 'career']):
         return json.dumps('PARAM ERROR')
 
     if request_data['career'] == config.career_project_manager:
-        data = project.get_project(uid=request_data['uid'], include_reject=True)
+        data = project.get_info(uid=request_data['uid'], include_reject=True)
     else:
-        data = project.get_project_include_work_time(request_data['uid'])
+        data = project.get_info_include_work_time(request_data['uid'])
 
     if has_error(data):
         return json.dumps('BACKEND ERROR')
@@ -26,12 +26,12 @@ def mine():
         return json.dumps(data)
 
 @project_access.route('/modify/show', methods=['GET'])
-def modify_show():
+def project_modify_show():
     request_data = get_value_dict()
     if not check_dict(request_data, ['id']):
         return json.dumps('PARAM ERROR')
 
-    data = project.get_project(project_id=request_data['id'], detail=True)
+    data = project.get_info(project_id=request_data['id'], detail=True)
 
     if has_error(data):
         return json.dumps('BACKEND ERROR')
@@ -39,7 +39,7 @@ def modify_show():
         return json.dumps(data)
 
 @project_access.route('/modify/save', methods=['POST'])
-def modify_save():
+def project_modify_save():
     request_data = get_value_dict()
     if not check_dict(request_data, ['id', 'name', 'describe', 'scheduled_time', 'delivery_day', \
                                      'project_superior_id', 'major_milestones', 'adopting_technology', \
@@ -56,7 +56,7 @@ def modify_save():
         return json.dumps({'status': data})
 
 @project_access.route('/create/show', methods=['GET'])
-def create_show():
+def project_create_show():
     request_data = get_value_dict()
     if not check_dict(request_data, ['uid']):
         return json.dumps('PARAM ERROR')
@@ -73,7 +73,7 @@ def create_show():
         return json.dumps(data)
 
 @project_access.route('/create/save', methods=['POST'])
-def create_save():
+def project_create_save():
     request_data = get_value_dict()
     if not check_dict(request_data, ['name', 'describe', 'development_type', 'scheduled_time', 'delivery_day', \
                                      'project_superior_id', 'custom_id', 'major_milestones', 'adopting_technology', \
@@ -90,7 +90,7 @@ def create_save():
         return json.dumps(data)
 
 @project_access.route('/work_time', methods=['GET'])
-def work_time():
+def project_work_time():
     request_data = get_value_dict()
     if not check_dict(request_data, ['uid', 'project_id']):
         return json.dumps('PARAM ERROR')
